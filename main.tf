cd087f70b2963e1a7cdb7dd8a4091e168fea30ce
Kevin Czarzasty <82130684+kczarzasty@users.noreply.github.com>
2021-08-22 15:25:38 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+# --- ec2 instance resources --- #
+resource "aws_instance" "tfimportec2" {
+ami = var.ami[var.region]
+#ami IS REGION SPECIFIC - please check which region you are in if you receive an error
+instance_type = var.instance_override == false ? var.instance_type : var.no_instance_type_change
+key_name = "tf-demo"
+}
+resource "aws_eip" "tfeip1" {
+vpc = true 
+tags = {
+  Name = "Tf-eip"
+ }
+}
+
+# --- EIPs ---
+resource "aws_eip_association" "eip_assoc1" {
+instance_id   = aws_instance.tfimportec2.id
+allocation_id = aws_eip.tfeip1.id
+}
+
+# --- Rds instance resource--- #
+resource "aws_db_instance" "tfdatabase" {
+engine         = "mysql"
+engine_version = "8.0.20"
+instance_class = "db.t2.micro"
+name           = "tf-import-mysql"
+allocated_storage = 20
+availability_zone = "us-east-1c"
+skip_final_snapshot = true
+backup_retention_period = 0
+apply_immediately = true
+username = var.username
