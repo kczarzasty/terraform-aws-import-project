ba27a84301957b9810a9ce36dd63ded78a5b1fa2
Kevin Czarzasty <82130684+kczarzasty@users.noreply.github.com>
2021-09-01 11:53:51 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,10 @@
-# these are variables
-
+# Parent Module Variables
+# Child module variables should be empty or default to false
+variable "instance_override" {
+type = bool
+default = true
+}
+variable "instance_type" {
+type = string
+default = "t3.micro"
+}
