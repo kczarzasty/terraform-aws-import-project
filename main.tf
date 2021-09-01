44e910af787698a724dd2da4e4c64da7bcde7055
Kevin Czarzasty <82130684+kczarzasty@users.noreply.github.com>
2021-09-01 17:01:16 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,30 +3,31 @@ resource "aws_instance" "tfimportec2" {
 ami = var.ami[var.region]
 #AMI is region-specific - confirm right region to avoid error
 instance_type = var.instance_override == false ? var.instance_type : var.no_instance_type_change
-key_name = "tf-demo"
+key_name = "MyPrivateKey"
 }
 resource "aws_eip" "tfeip1" {
-vpc = true 
+vpc = true
 tags = {
   Name = "Tf-eip"
  }
 }
 
-# EIPs 
+# EIPs
 resource "aws_eip_association" "eip_assoc1" {
 instance_id   = aws_instance.tfimportec2.id
 allocation_id = aws_eip.tfeip1.id
 }
 
-# RDS 
+# RDS
 resource "aws_db_instance" "tfdatabase" {
 engine         = "mysql"
 engine_version = "8.0.20"
 instance_class = "db.t2.micro"
 name           = "tf-import-mysql"
 allocated_storage = 20
-availability_zone = "us-east-1c"
+availability_zone = "us-east-1f"
 skip_final_snapshot = true
 backup_retention_period = 0
 apply_immediately = true
 username = var.username
+}
